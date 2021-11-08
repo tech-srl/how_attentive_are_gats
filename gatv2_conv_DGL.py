05d4b99c32314601e3457fd19011b21eff513b92
Shaked Brody <shakedbr@gmail.com>
2021-11-08 15:19:01 +0200

    Update gatv2_conv_DGL.py

--- gatv2_conv_DGL.py
+++ gatv2_conv_DGL.py
@@ -1,3 +1,5 @@
+
+"""Torch modules for graph attention networks v2 (GATv2)."""
 import torch as th
 from torch import nn
 
@@ -7,10 +9,30 @@ from dgl.base import DGLError
 from dgl.nn.pytorch.utils import Identity
 from dgl.utils import expand_as_pair
 
+
 class GATv2Conv(nn.Module):
     r"""
+    Description
+    -----------
+    Apply GATv2 from
+    `How Attentive are Graph Attention Networks? <https://arxiv.org/pdf/2105.14491.pdf>`__
+    over an input signal.
+    .. math::
+        h_i^{(l+1)} = \sum_{j\in \mathcal{N}(i)} \alpha_{ij}^{(l)} W^{(l)}_{right} h_j^{(l)}
+    where :math:`\alpha_{ij}` is the attention score bewteen node :math:`i` and
+    node :math:`j`:
+    .. math::
+        \alpha_{ij}^{(l)} &= \mathrm{softmax_i} (e_{ij}^{(l)})
+        e_{ij}^{(l)} &= \vec{a}^T^{(l)}\mathrm{LeakyReLU}\left(
+            W^{(l)}_{left} h_{i} + W^{(l)}_{right} h_{j}\right)
+    Parameters
+    ----------
     in_feats : int, or pair of ints
         Input feature size; i.e, the number of dimensions of :math:`h_i^{(l)}`.
+        If the layer is to be applied to a unidirectional bipartite graph, `in_feats`
+        specifies the input feature size on both the source and destination nodes.
+        If a scalar is given, the source and destination node feature size
+        would take the same value.
     out_feats : int
         Output feature size; i.e, the number of dimensions of :math:`h_i^{(l+1)}`.
     num_heads : int
@@ -32,12 +54,78 @@ class GATv2Conv(nn.Module):
         causing silent performance regression. This module will raise a DGLError if it detects
         0-in-degree nodes in input graph. By setting ``True``, it will suppress the check
         and let the users handle it by themselves. Defaults: ``False``.
-    bias (bool, optional): If set to :obj:`False`, the layer will not learn
+    bias : bool, optional
+        If set to :obj:`False`, the layer will not learn
         an additive bias. (default: :obj:`True`)
-    share_weights (bool, optional): If set to :obj:`True`, the same matrix
-        will be applied to the source and the target node of every edge.
+    share_weights : bool, optional
+        If set to :obj:`True`, the same matrix for :math:`W_{left}` and :math:`W_{right}` in
+        the above equations, will be applied to the source and the target node of every edge.
         (default: :obj:`False`)
-
+    Note
+    ----
+    Zero in-degree nodes will lead to invalid output value. This is because no message
+    will be passed to those nodes, the aggregation function will be applied on empty input.
+    A common practice to avoid this is to add a self-loop for each node in the graph if
+    it is homogeneous, which can be achieved by:
+    >>> g = ... # a DGLGraph
+    >>> g = dgl.add_self_loop(g)
+    Calling ``add_self_loop`` will not work for some graphs, for example, heterogeneous graph
+    since the edge type can not be decided for self_loop edges. Set ``allow_zero_in_degree``
+    to ``True`` for those cases to unblock the code and handle zero-in-degree nodes manually.
+    A common practise to handle this is to filter out the nodes with zero-in-degree when use
+    after conv.
+    Examples
+    --------
+    >>> import dgl
+    >>> import numpy as np
+    >>> import torch as th
+    >>> from dgl.nn import GATv2Conv
+    >>> # Case 1: Homogeneous graph
+    >>> g = dgl.graph(([0,1,2,3,2,5], [1,2,3,4,0,3]))
+    >>> g = dgl.add_self_loop(g)
+    >>> feat = th.ones(6, 10)
+    >>> gatv2conv = GATv2Conv(10, 2, num_heads=3)
+    >>> res = gatv2conv(g, feat)
+    >>> res
+    tensor([[[ 1.9599,  1.0239],
+            [ 3.2015, -0.5512],
+            [ 2.3700, -2.2182]],
+            [[ 1.9599,  1.0239],
+            [ 3.2015, -0.5512],
+            [ 2.3700, -2.2182]],
+            [[ 1.9599,  1.0239],
+            [ 3.2015, -0.5512],
+            [ 2.3700, -2.2182]],
+            [[ 1.9599,  1.0239],
+            [ 3.2015, -0.5512],
+            [ 2.3700, -2.2182]],
+            [[ 1.9599,  1.0239],
+            [ 3.2015, -0.5512],
+            [ 2.3700, -2.2182]],
+            [[ 1.9599,  1.0239],
+            [ 3.2015, -0.5512],
+            [ 2.3700, -2.2182]]], grad_fn=<GSpMMBackward>)
+    >>> # Case 2: Unidirectional bipartite graph
+    >>> u = [0, 1, 0, 0, 1]
+    >>> v = [0, 1, 2, 3, 2]
+    >>> g = dgl.heterograph({('A', 'r', 'B'): (u, v)})
+    >>> u_feat = th.tensor(np.random.rand(2, 5).astype(np.float32))
+    >>> v_feat = th.tensor(np.random.rand(4, 10).astype(np.float32))
+    >>> gatv2conv = GATv2Conv((5,10), 2, 3)
+    >>> res = gatv2conv(g, (u_feat, v_feat))
+    >>> res
+    tensor([[[-0.0935, -0.4273],
+            [-1.1850,  0.1123],
+            [-0.2002,  0.1155]],
+            [[ 0.1908, -1.2095],
+            [-0.0129,  0.6408],
+            [-0.8135,  0.1157]],
+            [[ 0.0596, -0.8487],
+            [-0.5421,  0.4022],
+            [-0.4805,  0.1156]],
+            [[-0.0935, -0.4273],
+            [-1.1850,  0.1123],
+            [-0.2002,  0.1155]]], grad_fn=<GSpMMBackward>)
     """
     def __init__(self,
                  in_feats,
@@ -50,8 +138,7 @@ class GATv2Conv(nn.Module):
                  activation=None,
                  allow_zero_in_degree=False,
                  bias=True,
-                 share_weights=False,
-                 **kwargs):
+                 share_weights=False):
         super(GATv2Conv, self).__init__()
         self._num_heads = num_heads
         self._in_src_feats, self._in_dst_feats = expand_as_pair(in_feats)
@@ -63,13 +150,13 @@ class GATv2Conv(nn.Module):
             self.fc_dst = nn.Linear(
                 self._in_dst_feats, out_feats * num_heads, bias=bias)
         else:
-        	self.fc_src = nn.Linear(
-	            self._in_src_feats, out_feats * num_heads, bias=bias)
-        	if share_weights:
-        		self.fc_dst = self.fc_src
-        	else:
-	            self.fc_dst = nn.Linear(
-	                self._in_src_feats, out_feats * num_heads, bias=bias)
+            self.fc_src = nn.Linear(
+                self._in_src_feats, out_feats * num_heads, bias=bias)
+            if share_weights:
+                self.fc_dst = self.fc_src
+            else:
+                self.fc_dst = nn.Linear(
+                    self._in_src_feats, out_feats * num_heads, bias=bias)
         self.attn = nn.Parameter(th.FloatTensor(size=(1, num_heads, out_feats)))
         self.feat_drop = nn.Dropout(feat_drop)
         self.attn_drop = nn.Dropout(attn_drop)
@@ -82,38 +169,40 @@ class GATv2Conv(nn.Module):
                 self.res_fc = Identity()
         else:
             self.register_buffer('res_fc', None)
-        self.reset_parameters()
         self.activation = activation
+        self.share_weights = share_weights
+        self.bias = bias
+        self.reset_parameters()
 
     def reset_parameters(self):
         """
-
         Description
         -----------
         Reinitialize learnable parameters.
-
         Note
         ----
         The fc weights :math:`W^{(l)}` are initialized using Glorot uniform initialization.
         The attention weights are using xavier initialization method.
         """
         gain = nn.init.calculate_gain('relu')
-        if hasattr(self, 'fc'):
-            nn.init.xavier_normal_(self.fc.weight, gain=gain)
-        else:
-            nn.init.xavier_normal_(self.fc_src.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_src.weight, gain=gain)
+        if self.bias:
+            nn.init.constant_(self.fc_src.bias, 0)
+        if not self.share_weights:
             nn.init.xavier_normal_(self.fc_dst.weight, gain=gain)
+            if self.bias:
+                nn.init.constant_(self.fc_dst.bias, 0)
         nn.init.xavier_normal_(self.attn, gain=gain)
         if isinstance(self.res_fc, nn.Linear):
             nn.init.xavier_normal_(self.res_fc.weight, gain=gain)
+            if self.bias:
+                nn.init.constant_(self.res_fc.bias, 0)
 
     def set_allow_zero_in_degree(self, set_value):
         r"""
-
         Description
         -----------
         Set allow_zero_in_degree flag.
-
         Parameters
         ----------
         set_value : bool
@@ -123,11 +212,9 @@ class GATv2Conv(nn.Module):
 
     def forward(self, graph, feat, get_attention=False):
         r"""
-
         Description
         -----------
         Compute graph attention network layer.
-
         Parameters
         ----------
         graph : DGLGraph
@@ -139,7 +226,6 @@ class GATv2Conv(nn.Module):
             :math:`(N_{in}, D_{in_{src}})` and :math:`(N_{out}, D_{in_{dst}})`.
         get_attention : bool, optional
             Whether to return the attention values. Default to False.
-
         Returns
         -------
         torch.Tensor
@@ -148,7 +234,6 @@ class GATv2Conv(nn.Module):
         torch.Tensor, optional
             The attention values of shape :math:`(E, H, 1)`, where :math:`E` is the number of
             edges. This is returned only when :attr:`get_attention` is ``True``.
-
         Raises
         ------
         DGLError
@@ -178,8 +263,11 @@ class GATv2Conv(nn.Module):
                 h_src = h_dst = self.feat_drop(feat)
                 feat_src = self.fc_src(h_src).view(
                     -1, self._num_heads, self._out_feats)
-                feat_dst = self.fc_dst(h_src).view(
-                    -1, self._num_heads, self._out_feats)
+                if self.share_weights:
+                    feat_dst = feat_src
+                else:
+                    feat_dst = self.fc_dst(h_src).view(
+                        -1, self._num_heads, self._out_feats)
                 if graph.is_block:
                     feat_dst = feat_src[:graph.number_of_dst_nodes()]
             graph.srcdata.update({'el': feat_src})# (num_src_edge, num_heads, out_dim)
